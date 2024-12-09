61a833df94fd7f885594fe4ac89f5e5297c94207
MHD-HSN-TD <8883597!@mhd>
2024-12-09 19:22:25 +0300

    make the Hook Pattern and update the API URL and make it HTTPS

---
+++ src/Hooks/usePrison.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+// const axios = require("axios");
+
+export const usePrison = () => {
+    const [searchedName, setSearchedName] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        const getPrison = async (name) => {
+            setLoading(true); //if get all data now remove the loading state
+            console.log("loading befor featching", loading);
+            // Define the data you want to send in the request body
+
+            // Define the URL for your GET request
+            // http://syrianprisoners-back.point-dev.net
+            const url = "https://syrianprisoners-back.point-dev.net/api/prisoner";
+
+            // Define the params to include in the request
+            const params = {
+                full_name: name,
+            };
+
+            try {
+                // Send a GET request with headers
+                const response = await axios.get(url, { params });
+
+                // Handle the response
+                let names = response.data;
+                setNames(names);
+                setLoading(false); //if get all data now remove the loading state
+                console.log("loading after featching", loading);
+            } catch (error) {
+                // Handle any errors
+                console.error("Error:", error);
+            }
+        };
+
+        setTimeout(() => getPrison(searchedName), 400);
+        // console.log("there is a change here ")
+        // sendDataWithHeaders();
+    }, [searchedName]);
+
+    const changeNameHandler = (e) => {
+        setTimeout(async () => {
+            setSearchedName(e.target.value);
+            console.log("firstName", searchedName);
+        }, 300);
+    };
+    return {
+        changeNameHandler,
+        searchedName,
+        setSearchedName,
+        loading,
+        setLoading,
+        names,
+        setNames,
+    };
+};
--- src/app/prisnores/page.jsx
+++ src/app/prisnores/page.jsx
@@ -2,96 +2,14 @@
 import Image from "next/image";
 import Table from "../../components/Table";
 import Link from "next/link";
-import { Filter } from "@/components/Filter";
-import { useEffect, useState } from "react";
-const axios = require('axios');
-
+import { usePrison } from "@/Hooks/usePrison";
 
 export default function Home() {
-
-  const [searchedName, setSearchedName] = useState('')
-  const [loading, setLoading] = useState(true);
-  const [names, setNames] = useState([])
-
-
-  // useEffect(() => {
-  //   // let featchedData = []
-
-  //   async function getUsers() {
-  //     setLoading(true) //set loading 
-  //     try {
-  //       const response = await axios.get('http://syrianprisoners-back.point-dev.net/api/prisoner');
-  //       const featchedNames = response.data.data
-  //       setNewData(featchedNames)
-  //       // console.log(newData);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //     finally {
-  //       setLoading(false) //if get all data now remove the loading state
-  //     }
-  //   }
-
-  //   console.log('getting all data')
-  //   getUsers()
-  // }, [])
-
-  useEffect(() => {
-
-    const getPrison = async (name) => {
-      setLoading(true) //if get all data now remove the loading state
-      console.log('loading befor featching', loading);
-      // Define the data you want to send in the request body
-
-
-      // Define the URL for your GET request
-      const url = 'http://syrianprisoners-back.point-dev.net/api/prisoner';
-
-      // Define the params to include in the request
-      const params = {
-        full_name: name,
-      };
-
-
-      try {
-        // Send a GET request with headers
-        const response = await axios.get(url, { params });
-
-        // Handle the response
-        let names = response.data
-        setNames(names)
-        setLoading(false) //if get all data now remove the loading state
-        console.log('loading after featching', loading);
-
-
-      } catch (error) {
-        // Handle any errors
-        console.error('Error:', error);
-      }
-      finally {
-      }
-
-    };
-
-    // setTimeout(() => getPrison(name, middleName, lastName), 3000)
-    setTimeout(() => getPrison(searchedName), 400)
-    // console.log("there is a change here ")
-    // sendDataWithHeaders();
-
-
-  }, [searchedName])
-
-
-
-
-  const changeNameHandler = (e) => {
-    setTimeout(async () => {
-      setSearchedName(e.target.value)
-      console.log("firstName", searchedName)
-    }, 300)
-  }
-
-
+  const {
+    changeNameHandler,
+    loading,
+    names,
+  } = usePrison() //Hook pattern
 
   return (
     <div>
